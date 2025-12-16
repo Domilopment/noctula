197df3aa08ed3c5375ad64655c26e1dfc48b489b
Dominic Narwutsch <dominic.narwutsch@gmail.com>
2025-12-16 15:26:26 +0100

    Add initramfs generation to build script
    Ensures the initramfs is generated with dracut for the specified kernel version during the build process. Sets appropriate permissions on the generated initramfs image.

--- build_files/build.sh
+++ build_files/build.sh
@@ -89,6 +89,13 @@ EOF
 
 systemctl enable podman.socket
 
+
+# Ensure Initramfs is generated
+export DRACUT_NO_XATTR=1
+/usr/bin/dracut --no-hostonly --kver "${KERNEL_VERSION}" --reproducible -v --add ostree -f "/lib/modules/${KERNEL_VERSION}/initramfs.img"
+chmod 0600 "/lib/modules/${KERNEL_VERSION}/initramfs.img"
+
+
 # cleanup stage
 # Clean temporary files
 rm -rf /tmp/*
